2b83dcd9409e69f6c0f26788f71e4922e84f8405
Wesley Smits <wesley@natuurhuisje.nl>
2022-04-18 12:42:49 +0200

    Clean up code

--- curtain.js
+++ curtain.js
@@ -1,17 +1,11 @@
+import { getAbsoluteHeight } from './utils.js   ';
+
 class Curtain extends HTMLDetailsElement {
     #summary = this.querySelector('summary');
 
     #content = this.querySelector('.curtain__content');
 
     #animation = null;
-
-    get open() {
-        return this.hasAttribute('open');
-    }
-
-    set open(value) {
-        this.toggleAttribute('open', value);
-    }
     
     connectedCallback() {
         this.#summary.addEventListener('click', this.#handleClick);
@@ -44,7 +38,7 @@ class Curtain extends HTMLDetailsElement {
 
     #expand() {
         const startHeight = `${this.offsetHeight}px`;
-        const endHeight = `${this.#summary.offsetHeight + this.#getAbsoluteHeight(this.#content)}px`;
+        const endHeight = `${this.#summary.offsetHeight + getAbsoluteHeight(this.#content)}px`;
 
         if (this.#animation) {
             this.#animation.cancel();
@@ -64,8 +58,6 @@ class Curtain extends HTMLDetailsElement {
     }
 
     #close() {
-        this.classList.add('closing');
-
         const startHeight = `${this.offsetHeight}px`;
         const endHeight = `${this.#summary.offsetHeight}px`;
 
@@ -84,26 +76,12 @@ class Curtain extends HTMLDetailsElement {
         );
 
         this.#animation.onfinish = () => this.#onAnimationFinish(false);
-        this.#animation.oncancel = () => {
-            this.classList.remove('closing');
-        };
     }
 
     #onAnimationFinish(open) {
         this.open = open;
         this.#animation = null;
-        this.classList.remove('closing');
-
         this.style.height = '';
-
-        this.dispatchEvent(new Event('toggle'));
-    }
-
-    #getAbsoluteHeight(el) {
-        const styles = window.getComputedStyle(el);
-        const margin = parseFloat(styles.marginTop) + parseFloat(styles.marginBottom);
-    
-        return Math.ceil(el.offsetHeight + margin);
     }
 }
 
